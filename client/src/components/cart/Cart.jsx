831956a77ab14f5b7aca0e16fe598df29e5aa138
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2020-12-17 19:53:15 -0600

    changed cart route

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -96,7 +96,7 @@ class Cart extends Component {
                       })
                       productsData = newProducts
                     }
-                    // this.fetchCart(newProducts)
+                    this.fetchCart(newProducts)
                     console.log('NEW PRODUCTS WITH IMAGES: ', this.state.filteredProducts)
                   }
                   // this.setState({
