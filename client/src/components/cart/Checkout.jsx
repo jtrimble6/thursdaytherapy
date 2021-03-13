7e04a1768e74b6d1fbf0941c4a1c6c3cae2b4f87
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-03-13 09:26:13 -0600

    trying to debug new payment location

--- client/src/components/cart/Checkout.jsx
+++ client/src/components/cart/Checkout.jsx
@@ -139,6 +139,7 @@ class Checkout extends Component {
         this.handleOrderSubmit = this.handleOrderSubmit.bind(this)
         this.sendNewOrderEmail = this.sendNewOrderEmail.bind(this)
         this.sendOrderConfirmationEmail = this.sendOrderConfirmationEmail.bind(this)
+        this.formatMoney = this.formatMoney.bind(this)
     
     }
 
@@ -611,7 +612,22 @@ class Checkout extends Component {
           })
         }
   
-
+    formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+          try {
+            decimalCount = Math.abs(decimalCount);
+            decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+        
+            const negativeSign = amount < 0 ? "-" : "";
+        
+            let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+            let j = (i.length > 3) ? i.length % 3 : 0;
+        
+            return '$' + negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+          } catch (e) {
+            // console.log(e)
+          }
+        }
+    
     scrollTop() {
         window.scrollTo({
           top: 0,
