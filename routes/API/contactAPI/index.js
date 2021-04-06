3206f6ecd270e178af6fb7e4fce6856ea061dfb8
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-04-06 13:32:31 -0500

    fixed email

--- routes/API/contactAPI/index.js
+++ routes/API/contactAPI/index.js
@@ -6,12 +6,12 @@ var nodemailer = require('nodemailer');
 require('dotenv').config();
 
 var transport = {
-  // host: 'smtp.gmail.com',
-  host: 'qvh6-ytnz.thursday-therapy.com',
+  host: 'smtp.gmail.com',
+  // host: 'qvh6-ytnz.thursday-therapy.com',
   auth: {
     port: 465,
     user: process.env.EMAIL_ADDRESS,
-    // pass: process.env.EMAIL_PASSWORD
+    pass: process.env.EMAIL_PASSWORD
   }
 }
 
