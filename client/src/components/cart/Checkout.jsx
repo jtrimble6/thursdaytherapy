47f7bd0793408a417fe4755aaad8b9e054511dde
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-12-29 13:38:26 -0600

    testing payment form

--- client/src/components/cart/Checkout.jsx
+++ client/src/components/cart/Checkout.jsx
@@ -149,7 +149,7 @@ class Checkout extends Component {
 
     renderCreditCardForm = () => {
       const config = {
-        applicationId: process.env.REACT_APP_SQUARE_PRODUCION_APPLICATION_ID,
+        applicationId: 'EAAAEQ82Eg5XrCGIQvU3PKGjCNrGtT9GX9OimPTQe5MMOePsUkuPp1gsbWMFQIOT',
         locationId: process.env.REACT_APP_LOCATION_ID,
         inputClass: "sq-input",
         autoBuild: false,
