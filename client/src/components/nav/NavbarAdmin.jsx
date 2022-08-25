59426b813297ed8726c856062dc9613a8725e1ea
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2022-08-25 13:16:40 -0500

    installed old rsuite version

--- client/src/components/nav/NavbarAdmin.jsx
+++ client/src/components/nav/NavbarAdmin.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, Nav } from 'rsuite';
 import NavbarHeaderImage from '../../css/images/navbarHeaderImage.png'
 
-import 'rsuite/styles/index.less';
+import 'rsuite/dist/styles/rsuite-default.css';
 import '../../css/nav/navbarHome.css'
 
 
