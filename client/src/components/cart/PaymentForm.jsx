e87ce22febc239fe4badf69402d9a2e5dae85cae
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2023-02-12 13:07:43 -0600

    updated location id

--- client/src/components/cart/PaymentForm.jsx
+++ client/src/components/cart/PaymentForm.jsx
@@ -697,19 +697,19 @@ export default class MyPaymentForm extends Component {
 						 * We strongly recommend use this function to verify the buyer and reduce
 						 * the chance of fraudulent transactions.
 						 */
-						createVerificationDetails={() => ({
-							amount: '1.00',
-							/* collected from the buyer */
-							billingContact: {
-							addressLines: ['123 Main Street', 'Apartment 1'],
-							familyName: 'Doe',
-							givenName: 'John',
-							countryCode: 'GB',
-							city: 'London',
-							},
-							currencyCode: 'GBP',
-							intent: 'CHARGE',
-						})}
+						// createVerificationDetails={() => ({
+						// 	amount: '1.00',
+						// 	/* collected from the buyer */
+						// 	billingContact: {
+						// 	addressLines: ['123 Main Street', 'Apartment 1'],
+						// 	familyName: 'Doe',
+						// 	givenName: 'John',
+						// 	countryCode: 'GB',
+						// 	city: 'London',
+						// 	},
+						// 	currencyCode: 'GBP',
+						// 	intent: 'CHARGE',
+						// })}
 						/**
 						* Identifies the location of the merchant that is taking the payment.
 						* Obtained from the Square Application Dashboard - Locations tab.
